62ee112ac08bb27b2d4e3c36b3eb90e1fcfbf90f
Junjie Chen <junjie.j.chen@intel.com>
2017-05-24 14:32:53 +0800

    refine code

--- src/main/java/generator/RowGenerator.java
+++ src/main/java/generator/RowGenerator.java
@@ -1,17 +1,20 @@
 package generator;
 
-import com.sun.jersey.api.ParamException;
-import database.Schema;
 import net.sf.jsqlparser.parser.CCJSqlParserUtil;
-import net.sf.jsqlparser.schema.Column;
 import net.sf.jsqlparser.statement.Statement;
 import net.sf.jsqlparser.statement.create.table.ColumnDefinition;
 import net.sf.jsqlparser.statement.create.table.CreateTable;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 public class RowGenerator {
-    private List<ColumnGenerator> cgs;
+    private long expectedRows = 1;
+    private List<ColumnGenerator> cgs = new ArrayList<ColumnGenerator>();
     private CreateTable createTableStat;
 
     public RowGenerator(String createTableSql) throws Exception {
@@ -25,6 +28,36 @@ public class RowGenerator {
             ColumnGenerator cg = new ColumnGenerator(columnDefinition.getColumnName(), columnDefinition.getColDataType());
             cgs.add(cg);
         }
+    }
+
+    public void setExpectedRows(long expectedRows) {
+        this.expectedRows = expectedRows;
+    }
+
+
+    //how to estimate?
+    public int getBytesInRow() {
+        return cgs.size() * 16;
+    }
+
+    public int generateData(String uri) throws Exception {
+        Configuration conf = new Configuration();
+        conf.set("fs.default.name", "hdfs://bdpe611n1:9001");
+        FileSystem hdfs = FileSystem.get(conf);
+        Path file = new Path(uri);
+
+        if (hdfs.exists(file)) {
+            hdfs.delete(file, true);
+        }
+
+        OutputStream os = hdfs.create(file);
+
+        for (long i = 0; i < expectedRows; i++) {
+            os.write(nextRow().getBytes());
+        }
+        os.close();
+        hdfs.close();
+        return 0;
     }
 
     public String nextRow() {
--- src/main/java/generator/SchemaGenerator.java
+++ src/main/java/generator/SchemaGenerator.java
@@ -1,15 +1,11 @@
 package generator;
 
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
-
-import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 
 public class SchemaGenerator {
-    private List<RowGenerator> rgs;
+    private List<RowGenerator> rgs = new ArrayList<RowGenerator>();
     public int scale = 1;
 
     public int addRowGenerator(RowGenerator rg) {
@@ -22,25 +18,16 @@ public class SchemaGenerator {
         return 0;
     }
 
-    public int generateData(String uri) throws Exception {
-        Configuration conf = new Configuration();
-        conf.set("fs.default.name", "hdfs://bdpe611n1:9001");
-        FileSystem hdfs = FileSystem.get(conf);
-        Path file = new Path(uri);
 
-        if (hdfs.exists(file)) {
-            hdfs.delete(file, true);
+    public void generateData(String dir) throws Exception {
+        // how to set expected rows for each table
+        for(RowGenerator rg : rgs) {
+            rg.generateData(dir);
         }
-
-        OutputStream os = hdfs.create(file);
-        // TODO write data here.
-        os.close();
-        hdfs.close();
-        return 0;
     }
 
     public static void main(String[] args) {
-        String sql = "create table (a int, b string)";
+        String sql = "create table test (a int, b STRING);";
         try {
             SchemaGenerator sg = new SchemaGenerator();
             sg.addRowGenerator(new RowGenerator(sql));
