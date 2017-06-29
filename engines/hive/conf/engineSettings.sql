93129d77ad8429ceb364512c5790424f38d469af
Junjie Chen <junjie.j.chen@intel.com>
2017-06-29 17:29:01 +0800

    add configuration to enable bloom filter

--- engines/hive/conf/engineSettings.sql
+++ engines/hive/conf/engineSettings.sql
@@ -2,6 +2,8 @@ set parquet.block.size=134217728;
 set dfs.block.size=134217728;
 set parquet.enable.bloom.filter=true;
 set parquet.bloom.filter.enabled=true;
+set hive.optimize.ppd=true;
+set hive.optimize.index.filter=true;
 
 CREATE DATABASE IF NOT EXISTS bloom_filter_data;
 use bloom_filter_data;
